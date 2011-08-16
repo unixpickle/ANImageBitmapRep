200ffdf2bf2fea1d45d6b97e2aaa2517df86e3ae
Alex Nichol <alexnichol@comcast.net>
2011-08-16 19:18:25 -0400

    Added simpler cropping method

--- CroppableBitmapRep.h
+++ CroppableBitmapRep.h
@@ -16,14 +16,28 @@
 /**
  * Cuts a part of the bitmap out for a new bitmap.
  * @param frame The rectangle from which a portion of the image will
- * be cut.  If this is the size of the image bitmap, the bitmap will.
+ * be cut.
  * The coordinates for this start at (0,0).
- * remain unchanged.
+ * @discussion The coordinates for this method begin in the bottom
+ * left corner.  For a coordinate system starting from the top
+ * left corner, use cropTopFrame: instead.
  */
 - (void)cropFrame:(CGRect)frame;
 
 /**
+ * Cuts a part of the bitmap out for a new bitmap.
+ * @param frame The rectangle from which a portion of the image will
+ * be cut.
+ * The coordinates for this start at (0,0).
+ * @discussion The coordinates for this method begin in the top
+ * left corner.  For a coordinate system starting from the bottom
+ * left corner, use cropFrame: instead.
+ */
+- (void)cropTopFrame:(CGRect)frame;
+
+/**
  * Creates a new CGImageRef by cutting out a portion of this one.
+ * This takes its behavoir from cropFrame.
  * @return An autoreleased CGImageRef that has been cropped from this
  * image.
  */
--- CroppableBitmapRep.m
+++ CroppableBitmapRep.m
@@ -24,6 +24,19 @@
 	CGContextRelease(newBitmap);
 }
 
+- (void)cropTopFrame:(CGRect)frame {
+	BMPoint size = [self bitmapSize];
+	// It's kind of rude to prevent them from doing something kind of cool, so let's not.
+	// NSAssert(frame.origin.x >= 0 && frame.origin.x + frame.size.width <= size.x, @"Cropping frame must be within the bitmap.");
+	// NSAssert(frame.origin.y >= 0 && frame.origin.y + frame.size.height <= size.y, @"Cropping frame must be within the bitmap.");
+	
+	CGContextRef newBitmap = [CGContextCreator newARGBBitmapContextWithSize:frame.size];
+	CGPoint offset = CGPointMake(-frame.origin.x, -(size.y - (frame.origin.y + frame.size.height)));
+	CGContextDrawImage(newBitmap, CGRectMake(offset.x, offset.y, size.x, size.y), [self CGImage]);
+	[self setContext:newBitmap];
+	CGContextRelease(newBitmap);
+}
+
 - (CGImageRef)croppedImageWithFrame:(CGRect)frame {
 	BMPoint size = [self bitmapSize];
 	// It's kind of rude to prevent them from doing something kind of cool, so let's not.
